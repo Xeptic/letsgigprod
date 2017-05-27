71b633b438e0493545c3034d89a4ffa54b9ddc91
Winston <Xeptic@users.noreply.github.com>
2017-05-27 01:19:01 +0100

    search update

--- JavaScriptBandSite.js
+++ JavaScriptBandSite.js
@@ -31,9 +31,37 @@ $("#venuesClose").click(function(){
     $(".featuredArtistsCols").removeClass("col-xs-3");
     $(".featuredArtistsCols").addClass("col-xs-6");
 });
+
+$("#jsTest").click( function(){
+    
+   $("#jsTest").hide(); 
+    
+});
+    
+$(function() {
+
+    var start = moment();
+    var end = moment().add(30,'day');
+
+    function cb(start, end) {
+        $('#datePicker span').html(start.format('D MMMM, YYYY') + ' - ' + end.format('D MMMM, YYYY'));
+    }
+    
+    $('#datePicker').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+    
+});
 });
 
-
-$(window).resize( function(){
-     $("#bandNameHeader").addClass("")
-});
--- search.php
+++ search.php
@@ -2,21 +2,23 @@
 <html lang="en">
 <head>
 <script src="src/"></script>
-    <title>Let's Gig - Setup your profile</title>
+    <title>Let's Gig - Search</title>
     <meta charset="utf-8">
-    <script type="text/javascript" src="dist/jquery.cropit.js"></script>
-    <script type="text/javascript" src="src/"></script>
-    
-    <script type="javascript" src="test/"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    
+     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="moment.js" type="text/javascript"></script>
     <script src="https://use.fontawesome.com/725e203c7d.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="bootstrap/css/bootstrap-social.css"></script>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=BioRhyme" rel="stylesheet"> 
     <script type="text/javascript" src="JavaScriptBandSite.js"></script>
+    <script src="bootstrap/css/bootstrap-social.css"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
+   
+    <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=BioRhyme" rel="stylesheet"> 
 <nav id="theNavBar" class="navbar navbar-inverse">
         <div class="container-fluid">
             <!--Logo -->
@@ -62,8 +64,131 @@
         </div>
     </nav>
 </head>
-    <body>
+<body>
+<div id="searchWrapper" class="container-fluid">
+<div class="container">
+     <form id="searchForm" method="post">
+      <h1 >Search</h1>
+       <div class="form-group">
+           <div class="">
+               <input id="searchField" type="search" class="form-control" placeholder="Search venues, artists and events">
+               <button type="submit">Search</button>
+               
+            <div class="row"> 
+                
+                <div class="col-xs-4">
+                <select class="selectpicker pickersResize">
+                   <option>All Catagories</option>
+                   <option>Artists</option>
+                   <option>Events</option>
+                   <option>Venues</option>
+                </select>
+                 <select class="selectpicker pickersResize">
+                     <option>All Locations</option>
+                     <option>London</option>
+                     <option>Birmingham</option>
+                     <option>Glasgow</option>
+                     <option>Liverpool</option>
+                     <option>Bristol	ENG</option>
+                     <option>Manchester</option>	
+                     <option>Sheffield</option>
+                     <option>Leeds</option>	<option>Edinburgh</option>
+                     <option>Leicester</option>
+                 </select>
+                <select class="selectpicker pickersResize">
+                    <option>All Genres</option>
+                 <option>Rock</option>
+                 <option>Metal</option>
+                 <option>Reggae</option>
+                 <option>Ska</option>
+                 <option>Hip-Hop</option>
+                 <option>Electronic</option>
+                 <option>Blues</option>
+                 <option>Jazz</option>
+                 <option>Rap</option>
+                 <option>Latin</option>
+                 <option>Country</option>
+                </select>
+                   </div>
+               <div class="col-xs-8">
+                 <div id="datePicker" class="pull-right">
+                     <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+                     <span></span> <b class="caret"></b>
+                 </div>
+               </div>
+            </div>
+               
+           </div>
+        </div>
+    </form>
+</div>
+<div class="container">
+    <div row="">
+    <table id="searchResultsTable">
+    <thead>
+     <th class="center" colspan="3">Search Results</th>    
+    </thead> 
+        <tbody>
+         <tr id="tableHeads">
+            <td>ARTIST/VENUE NAME</td>
+             <td>GENRE</td>
+             <td>LOCATION</td>
+        </tr>
+        <tr id="resultsRow1">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr>
+            <tr id="resultsRow2">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow3">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow4">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow5">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow6">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow7">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow8">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr><tr id="resultsRow9">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr>
+            <tr id="resultsRow10">
+            <td>**ARTIST/VENUEname</td>
+            <td>**Genre</td>
+            <td>**Location</td>
+        </tr>
+        
+        
+        </tbody>
+        
+    </table>
     
     
-    </body>
+    
+    
+    </div>
+    
+    </div>
+</div>
+</body>
 </html>
